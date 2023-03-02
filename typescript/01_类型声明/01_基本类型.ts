ebfad63345108fb849f1f63a3ce1444964824a9b
进阶滴小白 <819014215@qq.com>
2023-03-02 21:39:20 +0800

    1233

--- typescript/01_类型声明/01_基本类型.ts
+++ typescript/01_类型声明/01_基本类型.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: fqiangzs@163.com
  * @Date: 2023-03-01 23:23:28
- * @LastEditTime: 2023-03-01 23:27:05
- * @FilePath: d:\WorkSpace\前端\typescript\01_类型声明\01_基本类型.ts
+ * @LastEditTime: 2023-03-02 21:36:50
+ * @FilePath: \pixijsDemo1d:\WorkSpace\学习\learn\typescript\01_类型声明\01_基本类型.ts
  * @Description: 
  */
 
@@ -13,4 +13,4 @@ console.log(a);
 // a = "hello";    //报错，在ts中不能将字符串赋值给number类型
 
 //如果变量的声明和赋值是同时进行的，TS会自动对变量进行类型检测
-1231231
+console.log("this is a log!");
