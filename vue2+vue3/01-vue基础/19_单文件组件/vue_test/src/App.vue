bc4e19c6454363da8aeb842292f1c75d99d0222e
进阶滴小白 <819014215@qq.com>
2023-04-02 23:36:40 +0800

    单文件组件

---
+++ vue2+vue3/01-vue基础/19_单文件组件/vue_test/src/App.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Author: 进阶滴小白
+ * @Date: 2023-04-02 13:46:05
+ * @LastEditTime: 2023-04-02 16:30:43
+ * @Description: APP.vue作为组件的顶级
+-->
+<template>
+  <div>
+    <MySchool></MySchool>
+    <MyStudent></MyStudent>
+  </div>
+</template>
+
+<script>
+//引入组件
+import MySchool from './components/MySchool.vue'
+import MyStudent from './components/MyStudent.vue'
+export default {
+    name:"App", 
+    components:{
+      MySchool,
+      MyStudent
+    }
+}
+</script>
+
+<style>
+
+</style>
